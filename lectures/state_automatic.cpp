b6f451e75c1805f3d08b8b9781bcfb3a815c11a2
Bodja_n <bs@mt11.su>
2021-11-02 16:20:29 +0300

    automatic works

--- lectures/state_automatic.cpp
+++ lectures/state_automatic.cpp
@@ -11,9 +11,18 @@ bool DFA(string const &s) {
         else if (state == OUT && c == '\'') state = IN1;
         else if (state == OUT && c == '"') state = IN2;
     }
-    return state = OUT;
+    return state == OUT;
 }
 
 int main() {
+    string str1 = "'Строка ли я?'";
+    bool first = DFA(str1);
+    printf("%d\n",int(first));
+    string str2 = "'А я строка ?";
+    bool second = DFA(str2);
+    printf("%d\n",int(second));
+    string str3 = "";
+    bool fird = DFA(str3);
+    printf("%d\n",int(fird));
     return 0;
 }
